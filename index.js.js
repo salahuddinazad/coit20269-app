9937f9164584f19c6db641accfcac6fedbce0705
Salahuddin Azad <salahuddinalazad@yahoo.com>
2022-05-19 18:34:46 +1000

    Make it better

--- index.js.js
+++ index.js.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
 
+
 app.use(express.json())    // <==== parse request body as JSON ( Inbuilt to Express )
 app.use(cors());
 
